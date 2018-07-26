e7408d0ee0e3504145977684ab324094724ddf59
thegi01 <deokim77@gmail.com>
2018-07-26 13:09:39 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var router = require('./router/main')(app);
+var router = require('./router/views')(app);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');

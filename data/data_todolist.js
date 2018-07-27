fbc75f0dbe5ae1707640a4b1d859d7bb93f18612
thegi01 <deokim77@gmail.com>
2018-07-27 17:41:34 +0900

    Add files via upload

---
+++ data/data_todolist.js
@@ -0,0 +1,18 @@
+var data = [
+    {
+        'date': '20180727',
+        'item': [
+            'item 1',
+            'item 2',
+            'item 3'
+        ]
+    },
+    {
+        'date': '20180728',
+        'item': [
+            'item 1',
+            'item 2',
+            'item 3'
+        ]
+    }
+]

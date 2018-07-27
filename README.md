0bc60116a0f4ad7ef4233c5f3d4d57e399804958
thegi01 <deokim77@gmail.com>
2018-07-27 17:41:10 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # markup-set
-마크업 기본 세팅 진행 중
+마크업 기본 세팅
 
 
 # 참조 
 - http://expressjs.com/ko/
 - https://scotch.io/tutorials/use-ejs-to-template-your-node-application
+- https://www.codementor.io/naeemshaikh27/node-with-express-and-ejs-du107lnk6
+- https://stackoverflow.com/questions/12703098/how-to-get-a-json-file-in-express-js-and-display-in-view
--- server.js
+++ server.js
@@ -6,8 +6,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+app.use(express.static('public'));
+app.use(express.static('data'));
+
 var server = app.listen(3000, function(){
     console.log('Express server ahs started on port 3000');
 });
 
-app.use(express.static('public'));

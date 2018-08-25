330753e79d1b4b1d039283f3a4bef019657875c4
thegi <deokim77@gmail.com>
2018-08-25 21:02:37 +0900

    no message

--- router/views.js
+++ router/views.js
@@ -1,13 +1,13 @@
 module.exports = function(app){
     app.get('/', function(req, res){
-        res.render('index.html');
+        res.render('index', {user:'pullip'});
     });
     app.get('/about', function(req, res){
-        res.render('about.html');
+        res.render('about', {title:'About'});
     });
 
     // markup
     app.get('/todolist', function(req, res){
-        res.render('todolist.html');
+        res.render('todolist');
     });
 }
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.use(express.static('public'));
-app.use(express.static('data'));
+app.use( express.static('public') );
+app.use( express.static('data') );
 
 var server = app.listen(3000, function(){
     console.log('Express server ahs started on port 3000');
